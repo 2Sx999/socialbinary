902c92e88f81f25959ac21e03c906ad1eef8abd8
qq714772696 <714772696@qq.com>
2018-09-24 14:57:29 +0800

    完成项目搭建

---
+++ socialbinary-parent/socialbinary-base/src/main/java/cn/porkchop/socialbinary/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,20 @@
+package cn.porkchop.socialbinary.base.controller;
+
+import cn.porkchop.socialbinary.common.utils.Result;
+import cn.porkchop.socialbinary.common.utils.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理类
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+}
---
+++ socialbinary-parent/socialbinary-base/src/main/java/cn/porkchop/socialbinary/base/controller/LabelController.java
@@ -0,0 +1,78 @@
+package cn.porkchop.socialbinary.base.controller;
+
+import cn.porkchop.socialbinary.base.pojo.Label;
+import cn.porkchop.socialbinary.base.service.LabelService;
+import cn.porkchop.socialbinary.common.utils.Result;
+import cn.porkchop.socialbinary.common.utils.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 标签控制层
+ */
+@RestController
+@RequestMapping("/label")
+public class LabelController {
+    @Autowired
+    private LabelService labelService;
+
+    /**
+     * 查询全部列表
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
+    }
+
+    /**
+     * 根据ID查询标签
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findById(id));
+    }
+
+    /**
+     * 增加标签
+     *
+     * @param label
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Label label) {
+        labelService.add(label);
+        return new Result(true, StatusCode.OK, "增加成功");
+    }
+
+    /**
+     * 修改标签
+     *
+     * @param label
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label, @PathVariable String id) {
+        label.setId(id);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK, "修改成功");
+    }
+
+    /**
+     * 删除标签
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id) {
+        labelService.deleteById(id);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+}
